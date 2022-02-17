11c0c254755c46705bfbc205779f7279d1605ec8
Franco Valencia <franco.avalencia@gmail.com>
2022-02-17 13:26:52 -0600

    Removed BridgeHelper contract to reduce complexity

--- contracts/bridge/IMXBridge.sol
+++ contracts/bridge/IMXBridge.sol
@@ -1,20 +1,19 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 import "./SignatureChecker.sol";
-import "./IMXBridgeHelper.sol";
 import "./../tokens/IBridgeable.sol";
 
 /// @author Franco Valencia
 /// @title A Bridge contract between IMX and EVM compatible blockchains
-contract IMXBridge is Ownable, IMXBridgeHelper{
+contract IMXBridge is SignatureChecker, ReentrancyGuard, Ownable, IERC721Receiver{
 
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
@@ -138,4 +137,7 @@ contract IMXBridge is Ownable, IMXBridgeHelper{
         emit ERC721Bridged(_to, _tokenId, _bridgedTokenAddress);
     }
 
+    function onERC721Received(address, address, uint256, bytes memory) public pure override returns(bytes4) {
+       return this.onERC721Received.selector;
+    }
 }

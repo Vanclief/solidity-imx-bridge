85ae73a08672cbf5c30231cac24480fc665be86b
Franco Valencia <franco.avalencia@gmail.com>
2022-03-28 12:25:40 -0600

    Created ERC20 deploy script

---
+++ scripts/deploy/erc20.ts
@@ -0,0 +1,42 @@
+/** @format */
+
+import { ethers, run } from "hardhat";
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+
+  // We get the contract to deploy
+  const tokenName = "Faucet Token";
+  const tokenSymbol = "FCT";
+  const bridgeAddress = "0x58177e0ab101E45a85AeB9789bC800694F625a5e";
+
+  const BridgeableERC20 = await ethers.getContractFactory("BridgeableERC20");
+  const contract = await BridgeableERC20.deploy(
+    tokenName,
+    tokenSymbol,
+    bridgeAddress
+  );
+
+  await contract.deployed();
+
+  console.log("Contract deployed to:", contract.address);
+  console.log("Deployed Contract Address:", contract.address);
+  console.log("Verifying contract in 3 minutes...");
+  await sleep(60000 * 3);
+  await run("verify:verify", {
+    address: contract.address,
+    constructorArguments: [tokenName, tokenSymbol, bridgeAddress],
+  });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
